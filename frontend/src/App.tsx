0b2c8366584926d7536266e4a1d1248480ad6e63
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2023-03-03 13:20:05 -0800

    tailwind app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
+
+interface Todo {
+  useID: string;
+  id: string;
+  title: string;
+}
 
 function App() {
-  const [todos, setTodos] = useState(null);
+  const [todos, setTodos] = useState<[Todo] | null>(null);
 
   const fetchTodos = async () => {
     const res = await fetch("/api/todos");
-    const data = await res.json();
-    setTodos(data)
-  }
+    const { data }: { data: [Todo] } = await res.json();
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
 
   return (
-    <div className="flex">
-      <h1 className='text-red-500'>create react app</h1>
-      <button onClick={fetchTodos}>CLICK</button>
-      {JSON.stringify(todos)}
+    <div className="flex flex-col">
+      <div className="bg-blue-400 text-center py-2 mb-4">CREATE REACT APP</div>
+      <div className="flex justify-center">
+        {/* <button
+          className="py-2 px-2 rounded border bg-blue-200 hover:bg-blue-800 border-gray-200"
+          onClick={fetchTodos}
+        >
+          Fetch Data
+        </button> */}
+      </div>
+      <div className="flex flex-col">
+        {todos?.map((todo) => {
+          return <div className="border border-gray-200 py-1 px-1 text-center">{todo.title}</div>;
+        })}
+      </div>
     </div>
   );
 }
